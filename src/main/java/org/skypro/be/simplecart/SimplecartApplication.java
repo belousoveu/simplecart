90b15ec6d578b3b5fc203895702888ff1639018b
Belousoveu <belousov.eu@gmail.com>
2024-09-30 14:35:47 +0300

    Initial commit

---
+++ src/main/java/org/skypro/be/simplecart/SimplecartApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.be.simplecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/be/simplecart/SimplecartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.be.simplecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
